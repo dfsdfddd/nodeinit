be63bd2c3d3db2a79c4045cbed842bd3276eb5ec
刘武 <1004928208@qq.com>
2019-10-20 12:30:32 +0800

    视频4-3多中间件+添加前缀方法

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 const Koa = require('koa');
-const app = new Koa();
 const Router = require('koa-router')
 
-/**
- * koa async 的执行顺序 
- * koa的router就是一个中间件
- * 一个koa就是由多个中间件组件
- */
-app.use(async (ctx,next) => {
-  console.log(1)
-  await next()
-  console.log(2)
+const app = new Koa();
+const router = new Router();
+const userRouter = new Router({prefix:'/users'}) // 前缀方法
 
-})
-app.use(async (ctx,next) => {
-  console.log(3)
+// 多中间件
+const auth = async (ctx,next) => {
+  console.log(ctx.url)
+  if(ctx.url != "/users"){
+    ctx.throw(401)
+  }
   await next()
-  console.log(4)
+}
+
+
+/**
+ * 
+ */
+userRouter.get('/',auth, async (ctx,next) => {
+  ctx.body = '234'
 })
-app.use(async (ctx,next) => {
-  console.log(5);
+userRouter.post('/:id',auth, async (ctx,next) => {
+  ctx.body = 'id'+ ctx.params.id
 })
+userRouter.put('/:id',auth, async (ctx,next) => {
+  ctx.body = 'ids'+ ctx.params.id
+})
+userRouter.delete('/:id',auth, async (ctx,next) => {
+  ctx.body = 'delete' + ctx.params.id
+})
+
+ app.use(router.routes())
+ app.use(userRouter.routes())
 
 app.listen(4000);
