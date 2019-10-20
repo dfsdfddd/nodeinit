636825e48fc429539d8e3b48b5aa976a80ff7fa3
刘武 <1004928208@qq.com>
2019-10-20 22:23:22 +0800

    新增参数校验的中间件和error报错的中间件在pro和dev环境里面的处理方式

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,7 +3,7 @@ class UserCtl {
   find(ctx){
     // 设置响应头部
     // ctx.set("Allow","GET,POST")
-    // a.b 500
+    // a.b //500
     ctx.body = db
   }
   findbyId(ctx){
@@ -14,14 +14,28 @@ class UserCtl {
     ctx.body = db[ctx.params.id*1]
   }
   create(ctx){
+    ctx.verifyParams({
+      name:{type:"string",required:true},
+      age:{type:"number",required:false},
+    })
     db.push(ctx.request.body)
     ctx.body = ctx.request.body
   }
   update(ctx) {
+    if(ctx.params.id*1>=db.length){
+      ctx.throw(412,'先决条件失败，id不存在')
+    }
+    ctx.verifyParams({
+      name:{type:"string",required:true},
+      age:{type:"number",required:false},
+    })
     db[ctx.params.id*1] = ctx.request.body
     ctx.body = ctx.request.body
   }
   deleted(ctx){
+    if(ctx.params.id*1>=db.length){
+      ctx.throw(412,'先决条件失败，id不存在')
+    }
     db.splice(ctx.params.id*1,1)
     ctx.status = 204
   }
--- app/index.js
+++ app/index.js
@@ -1,24 +1,20 @@
 const Koa = require('koa');
-const bodyParser = require('koa-bodyparser')
+const bodyParser = require('koa-bodyparser') //获取路由参数json中间件
 const app = new Koa();
-
-const routing = require("./routers")
+const routing = require("./routers")  
+const error = require('koa-json-error') // 错误报错中间件
+const parameter = require("koa-parameter") //参数校验中间件
 
 /**
  * 在所有中间件的最前面定义一个处理错误中建立，他能处理500，和项目内部抛出的错误处理412
+ * 
+ * 使用koa-json-error中间件，然后pro环境没有返回stack参数，本地环境有 
  */
-app.use(async (ctx,next) => {
-  try {
-    await next()
-  } catch (err) {
-      ctx.status = err.status||err.statusCode||500
-      ctx.body = {
-        message: err.message
-      }
-  }
-})
-
+app.use(error({
+  postFormat:(e,{stack,...rest}) => process.env.NODE_ENV === 'pro' ? rest : {stack,...rest}
+}))
 app.use(bodyParser())
+app.use(parameter(app))
 routing(app)
 
 
