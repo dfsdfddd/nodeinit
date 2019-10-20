cd5590bc85005421158f696bd539f66dc7e7aa8f
刘武 <1004928208@qq.com>
2019-10-20 21:48:27 +0800

    手写的错误提示

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,9 +3,14 @@ class UserCtl {
   find(ctx){
     // 设置响应头部
     // ctx.set("Allow","GET,POST")
+    // a.b 500
     ctx.body = db
   }
   findbyId(ctx){
+    
+    if(ctx.params.id*1>=db.length){
+      ctx.throw(412,'先决条件失败，id不存在')
+    }
     ctx.body = db[ctx.params.id*1]
   }
   create(ctx){
--- app/index.js
+++ app/index.js
@@ -4,6 +4,20 @@ const app = new Koa();
 
 const routing = require("./routers")
 
+/**
+ * 在所有中间件的最前面定义一个处理错误中建立，他能处理500，和项目内部抛出的错误处理412
+ */
+app.use(async (ctx,next) => {
+  try {
+    await next()
+  } catch (err) {
+      ctx.status = err.status||err.statusCode||500
+      ctx.body = {
+        message: err.message
+      }
+  }
+})
+
 app.use(bodyParser())
 routing(app)
 
