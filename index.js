f06bb09cb2fc6abceeeb79bd890424b6cc6f3ba5
刘武 <1004928208@qq.com>
2019-10-20 11:48:38 +0800

    初始化 koa

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const Koa = require('koa');
 const app = new Koa();
 const Router = require('koa-router')
 
+/**
+ * koa async 的执行顺序 
+ * koa的router就是一个中间件
+ * 一个koa就是由多个中间件组件
+ */
 app.use(async (ctx,next) => {
   console.log(1)
   await next()
