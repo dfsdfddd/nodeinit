4fe9c1985889571d1354af3bdd17aa4dface5e37
刘武 <1004928208@qq.com>
2019-10-20 12:53:45 +0800

    增删改查 定义和接口返回

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const userRouter = new Router({prefix:'/users'}) // 前缀方法
 // 多中间件
 const auth = async (ctx,next) => {
   console.log(ctx.url)
-  if(ctx.url != "/users"){
-    ctx.throw(401)
-  }
+  // if(ctx.url != "/users"){
+  //   ctx.throw(401)
+  // }
   await next()
 }
 
@@ -19,19 +19,25 @@ const auth = async (ctx,next) => {
  * 
  */
 userRouter.get('/',auth, async (ctx,next) => {
-  ctx.body = '234'
+  ctx.body = [{name:'asdf'},{name:'24234'}]
+})
+userRouter.get('/:id',auth, async (ctx,next) => {
+  ctx.body = {name:'asdf'}
 })
 userRouter.post('/:id',auth, async (ctx,next) => {
-  ctx.body = 'id'+ ctx.params.id
+  ctx.body = {name:'asdf'}
 })
 userRouter.put('/:id',auth, async (ctx,next) => {
-  ctx.body = 'ids'+ ctx.params.id
+  ctx.body = {name:'asdf2'}
 })
 userRouter.delete('/:id',auth, async (ctx,next) => {
-  ctx.body = 'delete' + ctx.params.id
+  ctx.status = 204
 })
 
  app.use(router.routes())
  app.use(userRouter.routes())
+ app.use(userRouter.allowedMethods())// 响应options方法 告诉所支持的请求方法
+ //405 状态吗 支持请求方法 但是没有定义
+ // 501状态吗 根本就不支持请求方法
 
 app.listen(4000);
