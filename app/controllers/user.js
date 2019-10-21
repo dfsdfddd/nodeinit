06918c6863a1102b39ed41c8489576ade9c3deb7
刘武 <1004928208@qq.com>
2019-10-21 23:19:49 +0800

    用mongoose 增删该查

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,42 +1,35 @@
-const db = [{name:'舒服'}] //重启服务 内存的内容就没了
+const User = require('../models/users')
+
 class UserCtl {
-  find(ctx){
+  async find(ctx){
     // 设置响应头部
     // ctx.set("Allow","GET,POST")
     // a.b //500
-    ctx.body = db
+    ctx.body = await User.find();
   }
-  findbyId(ctx){
-    
-    if(ctx.params.id*1>=db.length){
-      ctx.throw(412,'先决条件失败，id不存在')
-    }
-    ctx.body = db[ctx.params.id*1]
+  async findbyId(ctx){
+    const user = await User.findById(ctx.params.id);
+    if(!user){ctx.throw(404,'用户不存在')};
+    ctx.body = user;
   }
-  create(ctx){
+  async create(ctx){
     ctx.verifyParams({
       name:{type:"string",required:true},
-      age:{type:"number",required:false},
     })
-    db.push(ctx.request.body)
-    ctx.body = ctx.request.body
+    const user = await new User(ctx.request.body).save()
+    ctx.body = user
   }
-  update(ctx) {
-    if(ctx.params.id*1>=db.length){
-      ctx.throw(412,'先决条件失败，id不存在')
-    }
+  async update(ctx) {
     ctx.verifyParams({
       name:{type:"string",required:true},
-      age:{type:"number",required:false},
     })
-    db[ctx.params.id*1] = ctx.request.body
-    ctx.body = ctx.request.body
+    const user = await User.findByIdAndUpdate(ctx.params.id,ctx.request.body)
+    if(!user){ctx.throw(404,'用不不存在')}
+    ctx.body = await User.findById(ctx.params.id)
   }
-  deleted(ctx){
-    if(ctx.params.id*1>=db.length){
-      ctx.throw(412,'先决条件失败，id不存在')
-    }
-    db.splice(ctx.params.id*1,1)
+  async deleted(ctx){
+    const user  = await User.findByIdAndDelete(ctx.params.id)
+    if(!user){ctx.throw(404,'用户不存在')}
     ctx.status = 204
   }
 }
