4796bd7da3df4cc4fae92acf34ee738ba76a7a19
刘武 <1004928208@qq.com>
2019-10-23 18:41:46 +0800

    使用koajwt 代替手写验证中间件

--- app/routers/user.js
+++ app/routers/user.js
@@ -1,4 +1,4 @@
-const jsonwebtoken = require('jsonwebtoken')
+const jwt = require('koa-jwt')
 const Router = require('koa-router')
 const router = new Router({prefix:'/users'}) // 前缀方法
 
@@ -6,17 +6,18 @@ const {find,findbyId,create,update,deleted,login,checkOwer} = require("../contro
 const {secret} = require('../config')
 
 // 多中间件
-const auth = async (ctx,next) => {
-  const {authorization = ''} = ctx.request.header; //当没有authorization就设置问空
-  const token = authorization.replace('Bearer ','')
-  try {
-    const user = jsonwebtoken.verify(token,secret)
-    ctx.state.user = user//固定放这个用户信息
-  } catch (err) {
-    ctx.throw(401,err.message)
-  }
-  await next()
-}
+// const auth = async (ctx,next) => {
+//   const {authorization = ''} = ctx.request.header; //当没有authorization就设置问空
+//   const token = authorization.replace('Bearer ','')
+//   try {
+//     const user = jsonwebtoken.verify(token,secret)
+//     ctx.state.user = user//固定放这个用户信息
+//   } catch (err) {
+//     ctx.throw(401,err.message)
+//   }
+//   await next()
+// }
+const auth = jwt({secret});
 router.get('/', find)
 router.get('/:id', findbyId)
 router.post('/', create)
