b9c566a54f432aa956a16043597aab0e27d14cea
刘武 <1004928208@qq.com>
2019-10-24 09:00:05 +0800

    koabody 上传文件

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 const Koa = require('koa');
-const bodyParser = require('koa-bodyparser') //获取路由参数json中间件
+const koaBody = require('koa-body') //获取路由参数json中间件
+const koaStatic = require('koa-static')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = new Koa();
 const routing = require("./routers")  
@@ -13,6 +15,9 @@ mongoose.connect(connectionStr,{ useNewUrlParser: true,useUnifiedTopology: true 
 mongoose.connection.on('error',console.error)
 
 
+
+
+app.use(koaStatic(path.join(__dirname,'public')));
 /**
  * 在所有中间件的最前面定义一个处理错误中建立，他能处理500，和项目内部抛出的错误处理412
  * 
@@ -21,7 +26,14 @@ mongoose.connection.on('error',console.error)
 app.use(error({
   postFormat:(e,{stack,...rest}) => process.env.NODE_ENV === 'pro' ? rest : {stack,...rest}
 }))
-app.use(bodyParser())
+app.use(koaBody({
+  multipart:true,//允许上传文件
+  formidable:{
+    uploadDir: path.join(__dirname, '/public/uploads'), // 设置保存文件的路径
+    keepExtensions: true, //保留扩展名 文件后缀
+  }
+}))
+
 app.use(parameter(app))
 routing(app)
 
